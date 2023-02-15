c0c4f274fe0d01c683898d3d8e89eeccb9e0fe68
Victor Salimov <victorsalimov@MacBook-Air-Victor.local>
2023-02-15 22:55:47 +0300

    Create db and schemas (Company, Employee), init db in app.

--- app_flow/__init__.py
+++ app_flow/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+from app_flow.db import db
+from app_flow.schema_db import Company, Employee
 from app_flow.register.views import blueprint as reg_blueprint
 
 
@@ -8,6 +10,11 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_pyfile('config.py')
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(reg_blueprint)
 
---
+++ app_flow/schema_db.py
@@ -0,0 +1,46 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app_flow.db import db
+
+
+class Company(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    type_company = db.Column(db.String, nullable=False)
+    company_name = db.Column(db.String, nullable=False)
+    company_email = db.Column(db.String, unique=True, nullable=True)
+    tax_identification_number = db.Column(db.Integer, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    def set_password(self, password: str) -> str:
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password, password)
+
+    def __repr__(self) -> str:
+        return "Company {}, id - {}".format(self.company_name, self.id)
+
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_id = db.Column(
+        db.Integer, db.ForeignKey(
+            'company.id', ondelete='CASCADE'
+        ), nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    patronymic = db.Column(db.String, nullable=False)
+    employee_email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def set_password(self, password: str) -> str:
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password, password)
+
+    def __repr__(self) -> str:
+        return "User {} {} {}, id - {}".format(
+            self.last_name, self.first_name, self.patronymic,
+            self.id)
